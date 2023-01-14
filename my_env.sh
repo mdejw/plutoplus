61533f9b976635fa20059014653dbfbb598b18a6
Marcin Dawidowicz <mdawidow@gmail.com>
2023-01-14 16:35:48 +0100

    Changes to build version v.35 of plutosdr-fw for plutoplus

---
+++ my_env.sh
@@ -0,0 +1,4 @@
+# Example environment variables for build the fw
+export CROSS_COMPILE=~/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-
+export VIVADO_SETTINGS="source /home/marcda/Xilinx/Vivado/2022.1/.settings64-Vivado.sh"
+
