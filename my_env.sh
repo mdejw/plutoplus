77cbcd04151a64a866de540cb25122f164fc9595
Marcin Dawidowicz <mdawidow@gmail.com>
2023-01-16 01:11:03 +0100

    update of my env

--- my_env.sh
+++ my_env.sh
@@ -1,4 +1,7 @@
 # Example environment variables for build the fw
-export CROSS_COMPILE=~/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-
-export VIVADO_SETTINGS="source /home/marcda/Xilinx/Vivado/2022.1/.settings64-Vivado.sh"
-
+export CROSS_COMPILE=/home/marcda/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-
+export VIVADO_SETTINGS="/home/marcda/Xilinx/Vivado/2022.1/.settings64-Vivado.sh"
+export VIVADO_VERSION="v2022.1"
+export ADI_IGNORE_VERSION_CHECK=1
+# for xsct utility
+export PATH=$PATH:~/Xilinx/Vitis/2022.1/bin/
